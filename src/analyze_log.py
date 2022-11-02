5be9302f85a301bbd36b4dcfa1614eaac9c4de26
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-11-02 14:37:06 -0300

    dev: using trackOrders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,19 +2,6 @@ import csv
 from src.track_orders import TrackOrders
 
 
-def extract_customer(customer: str, data):
-    customer_data = dict({
-        "name": customer,
-        "visitas": [],
-        "pedidos": [],
-    })
-    for i, cliente in enumerate(data["cliente"]):
-        if cliente == customer:
-            customer_data["visitas"].append(data["dia"][i])
-            customer_data["pedidos"].append(data["pedido"][i])
-    return customer_data
-
-
 def analyze_log(file_path: str):
     if not file_path.endswith(".csv"):
         raise NotImplementedError(f"Extensão inválida:'{file_path}'")
@@ -26,23 +13,28 @@ def analyze_log(file_path: str):
     })
 
     try:
+        analyze = TrackOrders()
         with open(file_path, encoding="utf-8") as file:
             file_reader = csv.reader(file)
             for c, p, d in file_reader:
                 data["cliente"].append(c)
                 data["pedido"].append(p)
                 data["dia"].append(d)
-        analyze = TrackOrders()
+                analyze.add_new_order(c, p, d)
         all_dishes = [dish for dish in data["pedido"] if dish not in all_dishes]
-        maria = extract_customer("maria", data)
-        maria_pratos = [ordem["pedido"] for ordem in data if ordem["cliente"] == "maria" and ordem["pedido"] not in maria_pratos]
         
+        # maria_pratos = [ordem["pedido"] for ordem in data if ordem["cliente"] == "maria" and ordem["pedido"] not in maria_pratos]
+        maria_favorite_dish = analyze.get_most_ordered_dish_per_customer("maria")
+        arnaldo_burger_eater = analyze.get_most_ordered_dish_per_customer("arnaldo")
+        joao_dishes_unordered = analyze.get_never_ordered_per_customer("joao")
+        joao_days_away = analyze.get_days_never_visited_per_customer("joao")
+
         with open("data/mkt_campaign.txt", "w") as file:
             file.write(
-                f"{maria_customer}\n",
-                f"{arnaldo_customer}\n",
-                f"{joao_dishes}\n",
-                f"{joao_days}\n",
+                f"{maria_favorite_dish}\n",
+                f"{arnaldo_burger_eater}\n",
+                f"{joao_dishes_unordered}\n",
+                f"{joao_days_away}\n",
             )
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{file_path}'")
