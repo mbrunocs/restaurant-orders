28bed32aab660130b8314d1c9583541d264500da
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-11-02 14:37:43 -0300

    dev: implementing TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,10 +1,25 @@
+def extract_customer(customer: str, data):
+    customer_data = dict({
+        "name": customer,
+        "visitas": [],
+        "pedidos": [],
+    })
+    for i, cliente in enumerate(data["cliente"]):
+        if cliente == customer:
+            customer_data["visitas"].append(data["dia"][i])
+            customer_data["pedidos"].append(data["pedido"][i])
+    return customer_data
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque.
+    def __init__(self):
+        self.orders = list()
+    
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
@@ -19,4 +34,7 @@ class TrackOrders:
         pass
 
     def get_least_busy_day(self):
+        pass
+
+    def get_dish_orders_per_customer(self, customer, dish):
         pass
